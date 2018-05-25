de0e34bde5e63c99895e9feb63427320a2b70b19
Ivan Guardado <dev.ivangc@gmail.com>
2018-05-25 18:00:09 +0200

    Adds a posible solution

--- src/model/Board.js
+++ src/model/Board.js
@@ -1,3 +1,52 @@
 module.exports = class Board {
 
+    constructor({ snake, width, height }) {
+        this.snake = snake
+        this.width = width
+        this.height = height
+        this.apple = null
+    }
+
+    update() {
+        if (this.snake.isAlive()) this.snake.move()
+        
+        if (this.snakeShouldEat()) {
+            this.snake.eat()
+            this.apple = null
+        }
+        if (!this.apple)
+            this.placeApple()
+    }
+
+    snakeShouldEat() {
+        const head = this.snake.getHead()
+        return this.apple && head[0] === this.apple[0] && head[1] === this.apple[1]
+    }
+
+    placeApple() {
+        const x = rand(0, this.width)
+        const y = rand(0, this.height)
+        const newApple = [x, y]
+        const isInsideSnake = this.snake.getPosition().some(position =>
+            position[0] === newApple[0] && position[1] === newApple[1])
+        if (isInsideSnake) this.placeApple()
+        else this.apple = newApple
+    }
+
+    hasCollision() {
+        const head = this.snake.getHead()
+        const x = head[0]
+        const y = head[1]
+        if (y < 0) return true
+        if (y >= this.height) return true
+        if (x >= this.width) return true
+        if (x < 0) return true
+        return false
+    }
+
+    getApplePosition() {
+        return this.apple
+    }
 }
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min)) + min;
--- src/model/Snake.js
+++ src/model/Snake.js
@@ -1,6 +1,65 @@
 module.exports = class Snake {
 
+    constructor({ initialDirection, initialPosition } = {}) {
+        this.direction = initialDirection
+        this.position = initialPosition
+        this.hasEaten = false
+        this.hasCollision = false
+    }
 
+    getDirection() {
+        return this.direction
+    }
+
+    setDirection(newDirection) {
+        if (this.direction === SnakeDirections.LEFT && newDirection === SnakeDirections.RIGHT) return
+        if (this.direction === SnakeDirections.RIGHT && newDirection === SnakeDirections.LEFT) return
+        if (this.direction === SnakeDirections.UP && newDirection === SnakeDirections.DOWN) return
+        if (this.direction === SnakeDirections.DOWN && newDirection === SnakeDirections.UP) return
+
+        this.direction = newDirection
+    }
+
+    getPosition() {
+        return this.position
+    }
+
+    getHead() {
+        return this.position[0]
+    }
+
+    move() {
+        const head = this.position[0];
+        const x = head[0]
+        const y = head[1]
+        let newHead = []
+
+        if (this.direction === SnakeDirections.LEFT) newHead = [x - 1, y]
+        if (this.direction === SnakeDirections.RIGHT) newHead = [x + 1, y]
+        if (this.direction === SnakeDirections.UP) newHead = [x, y - 1]
+        if (this.direction === SnakeDirections.DOWN) newHead = [x, y + 1]
+
+        this.checkBodyCollision(newHead)
+
+        this.position.unshift(newHead)
+
+        if (this.hasEaten) this.hasEaten = false
+        else this.position.pop()
+    }
+
+    checkBodyCollision(newHead) {
+        this.hasCollision = this.position.some(position => {
+            return position[0] === newHead[0] && position[1] === newHead[1]
+        })
+    }
+
+    isAlive() {
+        return !this.hasCollision
+    }
+
+    eat() {
+        this.hasEaten = true
+    }
 }
 
 module.exports.SnakeDirections = SnakeDirections = {
